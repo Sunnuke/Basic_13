2fb75fb5eae41d4fe3f1828b2b29606ec17a17cd
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-29 10:37:53 -0800

    Created BasicJava.java/class and Added a layout of the methods being used to manipulate arrays and integers

---
+++ BasicJava.java
@@ -0,0 +1,61 @@
+public class BasicJava {
+    // Print 1-255
+    public Integer Print1To255() {
+        
+    }
+    
+    // Print odd numbers between 1-255
+    public Integer PrintOdds1To255() {
+        
+    }
+    
+    // Print Sum
+    public Integer PrintSum() {
+        
+    }
+    
+    // Iterating through an array
+    public Integer IteratingArray() {
+        
+    }
+    
+    // Find Max
+    public Integer FindMax() {
+        
+    }
+    
+    // Get Average
+    public Integer GetAverage() {
+        
+    }
+    
+    // Array with Odd Numbers
+    public Integer ArrayWithOdds() {
+        
+    }
+    
+    // Greater Than Y
+    public Integer GreaterThanY() {
+        
+    }
+    
+    // Square the values
+    public Integer SquareValues() {
+        
+    }
+    
+    // Eliminate Negative Numbers
+    public Integer NoNegativeNums() {
+        
+    }
+    
+    // Max, Min, and Average
+    public Integer MaxMinAvg() {
+        
+    }
+    
+    // Shifting the Values in the Array
+    public Integer ShiftingValues() {
+        
+    }
+}
